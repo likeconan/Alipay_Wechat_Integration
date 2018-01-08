432cc86aa7e45f04fa07dc46f28fd4cd4a6438f2
Conan <liyijia@shinetechchina.com>
2018-01-08 17:13:02 +0800

    add body parser

--- Server_Demo/server.js
+++ Server_Demo/server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var alipay = require('./pay_alipay');
 var wechat = require('./pay_wechat');
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/alipay', alipay)
 app.use('/wechat', wechat)
